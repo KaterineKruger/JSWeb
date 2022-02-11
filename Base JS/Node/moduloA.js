1ee95ac8324e8865d8f1584efe2783286a5889f1
Katerine Krüger <katerine.kruger@gmail.com>
2022-02-11 15:58:21 -0300

    Node 01

---
+++ Base JS/Node/moduloA.js
@@ -0,0 +1,5 @@
+// Exportação de módulos
+
+this.ola = 'Fala pessoal'
+exports.bemVindo = 'Bem vindo ao node'
+module.exports.ateLogo = 'Até o próximo exemplo'
---
+++ Base JS/Node/moduloB.js
@@ -0,0 +1,8 @@
+// Exportação de módulos
+
+module.exports = {
+    bomDia: 'Bom dia',
+    boaNoite() {
+        return 'Boa noite'
+    }
+}
